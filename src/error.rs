db9d04ab06adf63c504989a0e07992f917f11ac5
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-04-25 00:15:21 -0500

    wip

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,7 @@ use thiserror::Error as TError;
 #[derive(TError, Debug)]
 #[non_exhaustive]
 pub enum Error {
-    #[error("mlua error")]
+    #[error(transparent)]
     LuaError(#[from] MLuaError),
     #[error("serde_json error")]
     SerdeJsonError(#[from] serde_json::Error),
