4c86d972236da085382fc9bb6e8a6ff57ba73f4b
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-05-12 22:15:52 -0500

    bump xx

--- src/bin.rs
+++ src/bin.rs
@@ -1,15 +1,21 @@
+#[cfg(feature = "cli")]
 #[macro_use]
 extern crate log;
 
-use std::process::exit;
-
+#[cfg(feature = "cli")]
 mod cli;
 
+#[cfg(feature = "cli")]
 #[tokio::main]
 async fn main() {
     env_logger::init();
     if let Err(err) = cli::run().await {
         error!("{err}");
-        exit(1);
+        std::process::exit(1);
     }
+}
+
+#[cfg(not(feature = "cli"))]
+fn main() {
+    panic!("cli feature is not enabled");
 }
