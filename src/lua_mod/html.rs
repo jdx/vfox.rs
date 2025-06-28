b514f0a0ca3de5528cbf98d4ae96a789571a5839
Risu <79110363+risu729@users.noreply.github.com>
2025-06-28 13:41:44 +1000

    test: ignore capital queries

--- src/lua_mod/html.rs
+++ src/lua_mod/html.rs
@@ -79,7 +79,7 @@ mod tests {
         lua.load(mlua::chunk! {
             local html = require("html")
             local doc = html.parse("<html><body><div id='t2' name='123'>456</div><DIV foo='bar'>222</DIV></body></html>")
-            local f = doc:find("DIV"):eq(0)
+            local f = doc:find("div"):eq(0)
             local s = doc:find("div"):eq(1)
             assert(s:text() == "222")
             assert(f:text() == "456")
